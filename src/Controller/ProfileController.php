69261c1224a3144fe1c99005aed8aac3cdb76359
RBAGES <bagesremy@gmail.com>
2022-06-04 12:50:38 +0200

    template pour erreurs 404 et pour la page de profil de l'utilisateur

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,19 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Service\Helpers;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProfileController extends AbstractController
-{
+/**
+ * @Route("/user")
+ */
+class ProfileController extends AbstractController {
     /**
-     * @Route("/user/{username}", name="app_profile")
+     * @Route("/{username}", name="app_profile")
      */
-    public function index(string $username): Response
-    {
+    public function index(string $username, Helpers $helper, UserRepository $ur): Response {
+        $user = $ur->findOneBy(['username'=>$username]);
+        if(!$user){
+            return $helper->error(404, 'Utilisateur introuvable', 'Cet utilisateur n\'existe pas!');
+        }
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
+            'user'=>$user
         ]);
     }
 }
--- src/Service/Helpers.php
+++ src/Service/Helpers.php
@@ -2,6 +2,14 @@
 
 namespace App\Service;
 
-class Helpers{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class Helpers extends AbstractController{
+
+    public function error(int $code, string $customTitle ='', string $customMessage=''){
+        return $this->render('errors/error'.$code.'.html.twig', [
+            'customTitle' => $customTitle,
+            'customMessage' => $customMessage
+        ]);
+    }
 }
