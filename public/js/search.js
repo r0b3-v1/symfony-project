b8572dd5316db9aa191e60a09eaf37b229ac4a8d
RBAGES <bagesremy@gmail.com>
2022-06-28 15:54:34 +0200

    search start

---
+++ public/js/search.js
@@ -0,0 +1,26 @@
+const searchButton = document.getElementById('search-link');
+const searchDiv = document.getElementById('search-modal');
+const typeSelect = document.getElementById('search-type-select');
+
+searchButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    toggleCollapse(searchDiv);
+});
+
+typeSelect.addEventListener('change', function(){
+    let selectedValue = this.options[this.selectedIndex].value;
+    for(const option of document.querySelectorAll('[data]')){
+        
+        if (option.getAttribute('data') != selectedValue){
+            toggleCollapse(option);
+        }
+    }
+    const divToCollapse = document.querySelector(`[data="${selectedValue}"]`);
+
+    toggleCollapse(divToCollapse);
+});
+
+function toggleCollapse(element){
+    element.classList.toggle('collapsable');
+    element.classList.toggle('collapsed');
+}
