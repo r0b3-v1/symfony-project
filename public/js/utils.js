405be47d3f7e5172fbb8f75139da599f6d917172
RBAGES <bagesremy@gmail.com>
2022-07-18 11:50:26 +0200

    bouton afficher/masquer mot de passe

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,4 +1,6 @@
 const triggers = document.querySelectorAll('[target][collapse-trigger]');
+const passInput = document.querySelectorAll('input[type="password"]');
+console.log(passInput);
 
 for (const trigger of triggers) {
     trigger.addEventListener('click', function (e) {
@@ -35,3 +37,30 @@ function HTMLFromString(str){
     div.innerHTML = str.trim();
     return div.firstChild;
 }
+
+//fonction qui permet d'ajouter un oeil pour afficher/cacher son mot de passe
+(function togglePassword(){
+    for (const input of passInput) {
+        const toggleVisibleButton = document.createElement('i');
+        const divContainer = input.parentElement;
+        const inputContainer = document.createElement('div');
+
+        inputContainer.append(input,toggleVisibleButton);
+        toggleVisibleButton.classList.add('fa-solid', 'fa-eye', 'password-toggle');
+        toggleVisibleButton.setAttribute('title','Afficher le mot de passe')
+        toggleVisibleButton.addEventListener('click', function(){
+            this.classList.toggle('fa-eye')
+            this.classList.toggle('fa-eye-slash')
+            if(input.getAttribute('type') === 'password'){
+                toggleVisibleButton.setAttribute('title','Masquer le mot de passe')
+                input.setAttribute('type', 'text');
+            }
+            else{
+                toggleVisibleButton.setAttribute('title','Afficher le mot de passe')
+                input.setAttribute('type', 'password');
+            }
+        })
+
+        divContainer.append(inputContainer);
+    }
+})()
